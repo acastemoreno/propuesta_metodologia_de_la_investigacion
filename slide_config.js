bc223bc27b9c335c2e8172b40c05d6c6e2852cc3
acastemoreno <acastemoreno@gmail.com>
2016-09-19 01:27:44 -0500

    all

---
+++ slide_config.js
@@ -0,0 +1,47 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Propuesta de Proyecto para Metodologia de la Investigacion', 
+     subtitle: 'Docente: D.Eng. Alberto Coronado Matutti', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/uni.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Castellano Moreno, Albert Roman', 
+     company: '20094091J', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   },{
+     name: 'Loarte Canales, Katherine Silene', 
+     company: '20122127J', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   },{
+     name: 'Morales Navarro, Luis Miguel', 
+     company: '20090022C', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   },{
+     name: 'Roncal Cotrina, Richard Jefferson', 
+     company: '20092095H', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   }]
+};
